046eaf1688d6736b041b0b71b30fb73343456356
Simon Bugert <simon.bugert@gmail.com>
2017-11-19 09:35:15 +0000

    Refactor theme variables to new format

--- packages/nausicaa/src/theme.js
+++ packages/nausicaa/src/theme.js
@@ -1,96 +1,109 @@
-/* NAUSICAA */
-module.exports.nausikamint = '#92EFE5';
-module.exports.nausikabubblesolid = 'rgba(255,255,255,0.7)';
+export default {
+  /* NAUSICAA */
+  nausikamint: '#92EFE5',
+  nausikabubblesolid: 'rgba(255,255,255,0.7)',
 
-/* COLLECTION COLORS*/
-module.exports.fernsehblau = '#4DA1CB';
-module.exports.fernsehblausubline = '#00A7FF';
-module.exports.fernsehverlauf = '#00ADFF, #FF6500';
-module.exports.fernsehblautransparent = 'rgba(0, 167, 255, 0.5)';
+  /* COLLECTION COLORS*/
+  Fernsehen: {
+    color: '#4DA1CB',
+    subline: '#00A7FF',
+    verlauf: '#00ADFF, #FF6500',
+    transparent: 'rgba(0, 167, 255, 0.5)',
+  },
 
-module.exports.essenorange = '#CB974D';
-module.exports.essenorangesubline = '#FFAA00';
-module.exports.essenverlauf = '#FF5200, #FFC300';
-module.exports.essenorangetransparent = 'rgba(255, 170, 0, 0.7)';
+  Essen: {
+    color: '#CB974D',
+    subline: '#FFAA00',
+    verlauf: '#FF5200, #FFC300',
+    transparent: 'rgba(255, 170, 0, 0.7)',
+  },
 
-module.exports.herzrot = '#CB4D77';
-module.exports.herzrotsubline = '#FF0000';
-module.exports.herzverlauf = '#FF0000, #FF009E';
-module.exports.herzrottransparent = 'rgba(255,0,0,0.7)';
+  Herz: {
+    color: '#CB4D77',
+    subline: '#FF0000',
+    verlauf: '#FF0000, #FF009E',
+    transparent: 'rgba(255,0,0,0.7)',
+  },
 
-module.exports.lustigrosa = '#CB4DB6';
-module.exports.lustigapricotsubline = '#FFB600';
-module.exports.lustigverlauf = '#FB00FF, #FFDB00';
-module.exports.lustigapricottransparent = 'rgba(255, 182, 0, 0.7)';
+  Lustig: {
+    color: '#CB4DB6',
+    subline: '#FFB600',
+    verlauf: '#FB00FF, #FFDB00',
+    transparent: 'rgba(255, 182, 0, 0.7)',
+  },
 
-module.exports.musikgelb = '#CBC14D';
-module.exports.musikgelbsubline = '#ECE00C';
-module.exports.musikverlauf = '#63BF44, #FF5900';
-module.exports.musikgelbtransparent = 'rgba(236,224,12,0.8)';
+  Musik: {
+    color: '#CBC14D',
+    subline: '#ECE00C',
+    verlauf: '#63BF44, #FF5900',
+    transparent: 'rgba(236,224,12,0.8)',
+  },
 
-module.exports.Musik = {
-  color: '#CBC14D',
-  subline: '#ECE00C',
-  verlauf: '#63BF44, #FF5900',
-  transparent: 'rgba(236,224,12,0.8)',
+  Studium: {
+    color: '#774DCB',
+    subline: '#7F00FF',
+    verlauf: '#0D00FF, #FF2800',
+    transparent: 'rgba(127, 0, 255, 0.7)',
+  },
+  Technologie: {
+    color: '#4DCBB6',
+    subline: '#00FFB2',
+    verlauf: '#C000FF, #00FF3E',
+    // technologiezitat: '#68c0ac, #327f6e', // TODO: used?
+    transparent: 'rgba(0, 255, 178, 0.5)',
+  },
+  Natur: {
+    color: '#77CB4D',
+    subline: '#00FF54',
+    verlauf: '#FFF700, #FF4D00',
+    transparent: 'rgba(0, 255, 84, 0.5)',
+  },
+  Zukunft: {
+    color: '#4D6DCB',
+    subline: '#00B3FF',
+    verlauf: '#3300FF, #00FF6D',
+    transparent: 'rgba(0, 179, 255, 0.7)',
+  },
+  Reisen: {
+    color: '#FF0059',
+    subline: '#CB4D79',
+    verlauf: '#30f, #FF0000',
+    transparent: 'rgba(203,77,121,0.7)',
+  },
+  Sex: {
+    color: '#CB4DA1',
+    subline: '#FF0070',
+    verlauf: '#FF0000, #DF00FF',
+    transparent: 'rgba(255, 0, 112, 0.7)',
+  },
+  Sport: {
+    color: '#4dAECB',
+    subline: '#00B3FF',
+    verlauf: '#3300FF, #FFD400',
+    transparent: 'rgba(0, 179, 255, 0.7)',
+  },
+  Nostalgie: {
+    subline: '#332828',
+    transparent: 'rgba(51, 40, 40, 0.7)',
+  },
+  /* KONTRASTE */
+  mausgrau: '#585252',
+  hellgrau: '#C1C1C1',
+  dunkelgrau: '#333333',
+  eierschale: '#F9FAFB',
+  black: '#000000',
+
+  /* TRANSPARENZEN */
+  transparent: 'rgba(255, 255, 255, 0)',
+  dunkelgrautransparent1: 'rgba(0,0,0,0.1)',
+  dunkelgrautransparent2: 'rgba(53,47,47,0.1)',
+  dunkelgrautransparent3: 'rgba(0,0,0,0.8)',
+
+  /* SOCIAL MEDIA */
+  facebookblau: '#3C3CDE',
+  twitterblau: '#8FD6EC',
+  whatsappgruen: '#06C34B',
+
+  /* VARIABLEN */
+  drehungverlauf: '135deg',
 };
-
-module.exports.studiumlila = '#774DCB';
-module.exports.studiumlilasubline = '#7F00FF';
-module.exports.studiumverlauf = '#0D00FF, #FF2800';
-module.exports.studiumlilatransparent = 'rgba(127, 0, 255, 0.7)';
-
-module.exports.technologiemint = '#4DCBB6';
-module.exports.technologiemintsubline = '#00FFB2';
-module.exports.technologieverlauf = '#C000FF, #00FF3E';
-module.exports.technologiezitat = '#68c0ac, #327f6e';
-module.exports.technologieminttransparent = 'rgba(0, 255, 178, 0.5)';
-
-module.exports.naturgruen = '#77CB4D';
-module.exports.naturgruensubline = '#00FF54';
-module.exports.naturverlauf = '#FFF700, #FF4D00';
-module.exports.naturgruentransparent = 'rgba(0, 255, 84, 0.5)';
-
-module.exports.zukunftblau = '#4D6DCB';
-module.exports.zukunftblausubline = '#00B3FF';
-module.exports.zukunftverlauf = '#3300FF, #00FF6D';
-module.exports.zukunftblautransparent = 'rgba(0, 179, 255, 0.7)';
-
-module.exports.reisenburgunder = '#FF0059';
-module.exports.reisenburgundersubline = '#CB4D79';
-module.exports.reisenverlauf = '#30f, #FF0000';
-module.exports.reisenburgundertransparent = 'rgba(203,77,121,0.7)';
-
-module.exports.sexpink = '#CB4DA1';
-module.exports.sexpinksubline = '#FF0070';
-module.exports.sexverlauf = '#FF0000, #DF00FF';
-module.exports.sexpinktransparent = 'rgba(255, 0, 112, 0.7)';
-
-module.exports.sportblau = '#4dAECB';
-module.exports.sportblausubline = '#00B3FF';
-module.exports.sportverlauf = '#3300FF, #FFD400';
-module.exports.sportblautransparent = 'rgba(0, 179, 255, 0.7)';
-
-module.exports.nostalgiegrausubline = '#332828';
-module.exports.nostalgiegrautransparent = 'rgba(51, 40, 40, 0.7)';
-
-/* KONTRASTE */
-module.exports.mausgrau = '#585252';
-module.exports.hellgrau = '#C1C1C1';
-module.exports.dunkelgrau = '#333333';
-module.exports.eierschale = '#F9FAFB';
-module.exports.black = '#000000';
-
-/* TRANSPARENZEN */
-module.exports.transparent = 'rgba(255, 255, 255, 0)';
-module.exports.dunkelgrautransparent1 = 'rgba(0,0,0,0.1)';
-module.exports.dunkelgrautransparent2 = 'rgba(53,47,47,0.1)';
-module.exports.dunkelgrautransparent3 = 'rgba(0,0,0,0.8)';
-
-/* SOCIAL MEDIA */
-module.exports.facebookblau = '#3C3CDE';
-module.exports.twitterblau = '#8FD6EC';
-module.exports.whatsappgruen = '#06C34B';
-
-/* VARIABLEN */
-module.exports.drehungverlauf = '135deg';
