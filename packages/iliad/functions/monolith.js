fff17186de8522a471b2351e7ccb9ef4cc1230d4
Simon Bugert <simon.bugert@gmail.com>
2017-12-10 22:05:29 +0000

    Refactor pagination routes and prepare data for template rendering

--- packages/iliad/functions/monolith.js
+++ packages/iliad/functions/monolith.js
@@ -10,10 +10,10 @@ const theme = Theme(functions.config());
 
 admin.initializeApp(functions.config().firebase);
 
+const pagerSize = functions.config().pager.size;
+
 const firestore = admin.firestore();
 const articles = firestore.collection('articles');
-
-const PAGE_SIZE = 3;
 
 app.use(cors);
 app.use(cookieParser);
@@ -22,49 +22,49 @@ app.get('/', (req, res) => {
   res.send('Start');
 });
 
+const paginationHandler = collectionName => (req, res) => {
+  const page = req.params.page || 1;
+  articles
+    .select()
+    .where('collection', '==', collectionName)
+    .get()
+    .then(emptySelectSnapshot =>
+      articles
+        .select('slug', 'headline', 'picture')
+        .orderBy('slug')
+        .where('collection', '==', collectionName)
+        .offset((page - 1) * pagerSize)
+        .limit(pagerSize)
+        .get()
+        .then(documentSnapshots => {
+          const articles = documentSnapshots.docs.map(article =>
+            article.data()
+          );
+          res.send({
+            pagination: {
+              currentPage: parseInt(page, 10),
+              pagerSize,
+              articleCount: emptySelectSnapshot.docs.length,
+            },
+            articles,
+          });
+        })
+    )
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('Something broke!');
+    });
+};
+
 const collections = functions.config().collections;
 Object.keys(collections).forEach(collectionName => {
   const collectionPath = collections[collectionName];
 
-  app.get(`/${collectionPath}`, (req, res) => {
-    articles
-      .orderBy('slug')
-      .where('collection', '==', collectionName)
-      .limit(PAGE_SIZE)
-      .get()
-      .then(documentSnapshots => {
-        const articles = documentSnapshots.docs.map(
-          article => article.data().slug
-        );
-        res.send(JSON.stringify(articles));
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).send(err);
-      });
-  });
+  app.get(`/${collectionPath}`, paginationHandler(collectionName));
 
-  app.get(`/${collectionPath}/:page(\\d+)`, (req, res) => {
-    articles
-      .orderBy('slug')
-      .where('collection', '==', collectionName)
-      .offset((req.params.page - 1) * PAGE_SIZE)
-      .limit(PAGE_SIZE)
-      .get()
-      .then(documentSnapshots => {
-        const articles = documentSnapshots.docs.map(
-          article => article.data().slug
-        );
-        res.send(JSON.stringify(articles));
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).send('Something broke!');
-      });
-  });
+  app.get(`/${collectionPath}/:page(\\d+)`, paginationHandler(collectionName));
 
   app.get(`/${collectionPath}/:slug`, (req, res) => {
-    // TODO: empty select() for collection length
     articles
       .doc(req.params.slug)
       .get()
